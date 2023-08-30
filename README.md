15d698595c5dabf2b1295daea1f87f96f38c42b6
anezih <90565940+anezih@users.noreply.github.com>
2023-08-30 23:06:06 +0300

    +
    - added a check for non-dictionary mobi source files.
    - added an option (--opf) to parse metadata from accompanying opf file

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Convert unpacked MOBI dictionaries to StarDict input formats: Babylon Glossary S
 # Usage
 `python.exe mobi2stardict.py --help`
 ```
-usage: mobi2stardict.py [-h] [--html-file HTML_FILE] [--fix-links] [--dict-name DICT_NAME] [--author AUTHOR]
-                            [--gls] [--textual] [--chunked]
+usage: mobi2stardict.py [-h] [--html-file HTML_FILE] [--fix-links] [--dict-name DICT_NAME] [--author AUTHOR] [--gls]
+                        [--textual] [--chunked] [--opf]
 
 Convert unpacked Kindle MOBI dictionary files (book.html or part00000.html) to Babylon Glossary source files (.gls) or
 to Stardict Textual Dictionary Format. These source files can later be converted to StarDict format via StarDict
 Editor. Textual xml format can be converted to a wide-range of formats directly via PyGlossary. You can unpack MOBI
 files via 'KindleUnpack' or its Calibre plugin. Alternatively, you can use mobitool from libmobi project.
 
-optional arguments:
+options:
   -h, --help            show this help message and exit
   --html-file HTML_FILE
                         Path of the HTML file.
@@ -23,18 +23,19 @@ optional arguments:
   --gls                 Convert dictionary to Babylon glossary source.
   --textual             Convert dictionary to Stardict Textual Dictionary Format.
   --chunked             Parse html in chunks to reduce memory usage.
+  --opf                 Try to get the relevant metadata from the opf file.
 ```
 You need to install [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup) and [lxml](https://lxml.de/installation.html) packages to run the script.
 To convert the unpacked MOBI file to both GLS and Textual format you would call the script like this (assuming part00000.html is in the same directory with the script):
 ````
-python.exe mobi2stardict.py --fix-links --dict-name "Name of the dictionary" --author "Author" --gls --textual
+python.exe mobi2stardict.py --fix-links --dict-name "Name of the dictionary" --author "Author" --gls --textual --opf
 ````
 Change name and author accordingly.
 
 Also, while converting particularly large files you may want to pass the `--chunked` option to bring down the memory usage to more moderate levels. Then the line would become:
 ````
-python.exe mobi2stardict.py --fix-links --dict-name "Name of the dictionary" --author "Author" --gls --textual --chunked
+python.exe mobi2stardict.py --fix-links --dict-name "Name of the dictionary" --author "Author" --gls --textual --chunked --opf
 ````
 
 # NOTE
-You may come across some malformatted dictionaries that may result in inability to parse definitions.
+You may come across some poorly formatted dictionaries that may result in inability to parse definitions.
--- mobi2stardict.py
+++ mobi2stardict.py
@@ -1,7 +1,13 @@
-from bs4 import BeautifulSoup
 from dataclasses import dataclass
+from datetime import datetime
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+
 import argparse
+import glob
 import re
+import os
 import sys
 
 @dataclass
@@ -10,6 +16,67 @@ class Entry:
     INFL: set[str]
     BODY: str
 
+@dataclass
+class Metadata:
+    Title: str
+    Description: str
+    Creator: str
+    Date: str
+    InLang: str
+    OutLang: str
+
+def get_metadata(path: str) -> Metadata:
+    def soup_gettext(_find) -> str:
+        if _find:
+            return _find.text.strip()
+        return ""
+    _base_dir = None
+    if path == "part00000.html":
+        _base_dir = os.getcwd()
+    else:
+        _base_dir = str(Path(path).parent.absolute().resolve())
+    opf_path = glob.glob(os.path.join(_base_dir, "*.opf"))[0]
+    try:
+        with open(opf_path, "r", encoding="utf-8") as f:
+            opf_soup = BeautifulSoup(f.read(), "lxml-xml")
+    except:
+        print("Could not read opf file.")
+        return Metadata("","","","","","")
+    return Metadata(
+        Title       = soup_gettext(opf_soup.find("dc:title")),
+        Description = soup_gettext(opf_soup.find("dc:description")),
+        Creator     = soup_gettext(opf_soup.find("dc:creator")),
+        Date        = soup_gettext(opf_soup.find("dc:date")),
+        InLang      = soup_gettext(opf_soup.find("DictionaryInLanguage")),
+        OutLang     = soup_gettext(opf_soup.find("DictionaryOutLanguage"))
+    )
+
+def set_metadata(_key: str, _metadata: Metadata, dict_name: str, author: str):
+    if _key == 'Title':
+        if _metadata.Title:
+            _title = f"{_metadata.Title}"
+            if _metadata.InLang:
+                _title += f" ({_metadata.InLang.replace('-','_')}"
+            if _metadata.OutLang:
+                _title += f"-{_metadata.OutLang.replace('-','_')})"
+            else:
+                _title += f")"
+            return _title
+        else:
+            return dict_name
+    if _key == 'Desc':
+        return _metadata.Description
+    if _key == 'Creator':
+        if _metadata.Creator:
+            return _metadata.Creator
+        else:
+            return author
+    if _key == 'Date':
+        if _metadata.Date:
+            return _metadata.Date
+        else:
+            return f"{datetime.today().strftime('%d/%m/%Y')}"
+
 def fix(body_str: str) -> str:
     temp = BeautifulSoup(body_str, "lxml")
     links = temp.find_all("a", href=True)
@@ -17,12 +84,15 @@ def fix(body_str: str) -> str:
         body_str = re.sub(link["href"], f"bword://{link.getText()}", body_str)
     return body_str
 
-def convert(html: str, dict_name: str, author: str, fix_links: bool, gls: bool, textual: bool, chunked: bool) -> None:
+def convert(html: str, dict_name: str, author: str, fix_links: bool,
+            gls: bool, textual: bool, chunked: bool, opf: bool) -> None:
     try:
         with open(html, "r", encoding="utf-8") as f:
             book = f.read()
     except FileNotFoundError:
         sys.exit("Could not open the file. Check the filename.")
+    if "<idx:" not in book:
+        sys.exit("Not a dictionary file.")
     entry_groups = []
     if chunked:
         cnt = 0
@@ -40,6 +110,7 @@ def convert(html: str, dict_name: str, author: str, fix_links: bool, gls: bool, 
     else:
         entry_groups.append(book)
     arr = []
+    cnt = 0
     for group in entry_groups:
         soup = BeautifulSoup(group, "lxml")
         entries = soup.find_all("idx:entry")
@@ -83,10 +154,16 @@ def convert(html: str, dict_name: str, author: str, fix_links: bool, gls: bool, 
                     arr.append(Entry(headword, inflections_set, body))
                 else:
                     arr.append(Entry(headword, {}, body))
-
+                cnt += 1
+                print("> Parsed", f"{cnt:,}", "entries.", end="\r")
+    print()
+    meta = get_metadata(path=html)
     if gls:
+        gls_metadata  = f"\n#stripmethod=keep\n#sametypesequence=h\n"
+        gls_metadata += f"#bookname={set_metadata('Title',meta,dict_name,author)}\n"
+        gls_metadata += f"#author={set_metadata('Creator',meta,dict_name,author)}\n\n"
         with open("book.gls", "w", encoding="utf-8") as d:
-            d.write(f"\n#stripmethod=keep\n#sametypesequence=h\n#bookname={dict_name}\n#author={author}\n\n")
+            d.write(gls_metadata)
             for entry in arr:
                 if entry.INFL:
                     headwords = f"{entry.HW}|{'|'.join(entry.INFL)}"
@@ -94,6 +171,7 @@ def convert(html: str, dict_name: str, author: str, fix_links: bool, gls: bool, 
                     headwords = entry.HW
                 single_def = f"{headwords}\n{entry.BODY}\n\n"
                 d.write(single_def)
+                d.flush()
     if textual:
         from lxml import etree as ET
         from datetime import datetime
@@ -102,12 +180,12 @@ def convert(html: str, dict_name: str, author: str, fix_links: bool, gls: bool, 
 
         info     = ET.SubElement(root, "info")
         version  = ET.SubElement(info, "version").text = "3.0.0"
-        bookname = ET.SubElement(info, "bookname").text = f"{dict_name}"
-        author_  = ET.SubElement(info, "author").text = f"{author}"
-        desc     = ET.SubElement(info, "description").text = ""
+        bookname = ET.SubElement(info, "bookname").text = set_metadata('Title',meta,dict_name,author)
+        author_  = ET.SubElement(info, "author").text = set_metadata('Creator',meta,dict_name,author)
+        desc     = ET.SubElement(info, "description").text = set_metadata('Desc',meta,dict_name,author)
         email    = ET.SubElement(info, "email").text = ""
         website  = ET.SubElement(info, "website").text = ""
-        date     = ET.SubElement(info, "date").text = f"{datetime.today().strftime('%d/%m/%Y')}"
+        date     = ET.SubElement(info, "date").text = set_metadata('Date',meta,dict_name,author)
         dicttype = ET.SubElement(info, "dicttype").text = ""
 
         for entry in arr:
@@ -136,20 +214,22 @@ if __name__ == '__main__':
     """)
     parser.add_argument('--html-file', default='part00000.html',
         help="Path of the HTML file.")
-    parser.add_argument('--fix-links', action='store_true', 
+    parser.add_argument('--fix-links', action='store_true',
         help="Try to convert in-dictionary references to glossary format.")
-    parser.add_argument('--dict-name', default="part00000", 
+    parser.add_argument('--dict-name', default="part00000",
         help="Name of the dictionary file.")
     parser.add_argument('--author', default="author",
         help="Name of the author or publisher.")
-    parser.add_argument('--gls', action='store_true', 
+    parser.add_argument('--gls', action='store_true',
         help="Convert dictionary to Babylon glossary source.")
-    parser.add_argument('--textual', action='store_true', 
+    parser.add_argument('--textual', action='store_true',
         help="Convert dictionary to Stardict Textual Dictionary Format.")
-    parser.add_argument('--chunked', action='store_true', 
-        help="Parse html in chunks to reduce memory usage.") 
+    parser.add_argument('--chunked', action='store_true',
+        help="Parse html in chunks to reduce memory usage.")
+    parser.add_argument('--opf', action='store_true',
+        help="Try to get the relevant metadata from the opf file.")
     args = parser.parse_args()
     if not (args.gls or args.textual):
-        print("You need to specify at least 1 output format: --gls, --textual or both.")
-        sys.exit()
-    convert(args.html_file, args.dict_name, args.author, args.fix_links, args.gls, args.textual, args.chunked)
+        sys.exit("You need to specify at least 1 output format: --gls, --textual or both.")
+    convert(args.html_file, args.dict_name, args.author, args.fix_links,
+            args.gls, args.textual, args.chunked, args.opf)
